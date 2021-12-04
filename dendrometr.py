e4c27a735e14929af6b4cb41c4930c2bf607cc0d
Piotr Baran <piotrbaran0123@gmail.com>
2021-12-04 09:15:31 +0100

    V module is work - final

--- dendrometr.py
+++ dendrometr.py
@@ -1,72 +1,81 @@
 import dendro_formulas
 
-
 print('Dendrometr 1.0.1')
 print('')
 print('Hi User!')
 
-
-class start:
-    print('Moduły:')
-    print('A. Moduł obliczania miąższości drzewa - met. proste')
-    print('B. Moduł obliczania miąższości - met. sekcyjne')
-    print('X. Wyjście')
-    print('')
-
-
-tryb = 0
-
-
-class modules:
-    tryb = input('Wybierz moduł: ')
-
-        while tryb == 'A' or tryb == 'B' or tryb == 'X':
-            if tryb == 'A':
-                print('Tryby:')
-                print('A. wzór Hubera')
-                print('B. wzór Smaliana')
-                print('C. Wzór Hossfelda')
-                print('D. Wzór Newtona')
-                module = str(input('Wybierz tryb: '))
-                if module == 'A':
-                    print(dendro_formulas.VHub(d1_2=int(input('Wpisz d1/2 (cm): ')),
-                                               h=int(input('Wpisz h (m): '))))
-                    print(start)
-                    print(modules)
-                elif module == 'B':
-                    print(dendro_formulas.VSmal(d0=int(input('Wpisz d0 (cm): ')),
-                                                dl=int(input('Wpisz dl (cm): ')),
-                                                h=int(input('Wpisz h (m): '))))
-                    print(start)
-                    print(modules)
-                elif module == 'C':
-                    print(dendro_formulas.VHoss(d1_3=int(input('Wpisz d1_3 (cm): ')),
-                                                dl=int(input('Wpisz dl (cm): ')),
-                                                h=int(input('Wpisz h (m): '))))
-                    print(start)
-                    print(modules)
-                elif module == 'D':
-                    print(dendro_formulas.VNew(d0=int(input('Wpisz 0 (cm): ')),
-                                               d1_2=int(input('Wpisz d1_2 (cm): ')),
-                                               dl=int(input('Wpisz dl (cm): ')),
-                                               h=int(input('Wpisz h (m): '))))
-                    print(start)
-                    print(modules)
-                elif module == 'X':
-                    print(start)
-                    print(modules)
-                else:
-                    print('Error - back to start')
-                    print(start)
-                    print(modules)
-            elif tryb == 'B':
-                print(dendro_formulas.Vsek)
-            elif tryb == 'X':
-                pass
+class main:
+    module = 'Run'
+    while module == 'A' or module == 'B' or module == 'X' or 'Run':
+        if module == 'Run':
+            print('Modules:')
+            print('A. Module tree volume calculating - simple mod.')
+            print('B. Module tree volume calculating - section mod.')
+            print('X. Exit')
+            print('')
+            module = input('Choose your module: ')
+        elif module == 'A':
+            print('Module tree volume calculating - simple mod. ')
+            print('Models:')
+            print("A. Huber's model")
+            print("B. Smalian's model")
+            print("C. Hossfeld's model")
+            print("D. Newton's model")
+            print('X. Return')
+            mode = str(input('Choose model: '))
+            if mode == 'A':
+                print(dendro_formulas.VHub(d1_2=int(input('Write diameter in half lenght of tree = d1/2 (cm): ')),
+                                           h=int(input('Write lenght of tree = h (m): '))))
+                module = 'A'
+            elif mode == 'B':
+                print(dendro_formulas.VSmal(d0=int(input('Write base diameter = d0 (cm): ')),
+                                            dl=int(input('Write top diameter = dl (cm): ')),
+                                            h=int(input('Write lenght of tree = h (m): '))))
+                module = 'A'
+            elif mode == 'C':
+                print(dendro_formulas.VHoss(d1_3=int(input('Write diameter in 1/3 lenght of tree = d1/3 (cm): ')),
+                                            dl=int(input('Write top diameter = dl (cm): ')),
+                                            h=int(input('Write lenght of tree = h (m): '))))
+                module = 'A'
+            elif mode == 'D':
+                print(dendro_formulas.VNew(d0=int(input('Write diameter in base of tree = 0 (cm): ')),
+                                           d1_2=int(input('Write diameter in half lenght of tree = d1/2 (cm): ')),
+                                           dl=int(input('Write top diameter = dl (cm): ')),
+                                           h=int(input('Write lenght of tree = h (m): '))))
+                module = 'A'
+            elif mode == 'X':
+                module = 'Run'
             else:
-                print('Error - back to start')
-                print(start)
-                print(modules)
+                print('Error')
+                module = 'Run'
+        elif module == 'B':
+            print('Module tree volume calculating - section mod.')
+            print('Models: ')
+            print("A. Huber's model")
+            print("B. Samlian's model")
+            print('X. Return')
+            mode = str(input('Choose your model: '))
+            if mode == 'A':
+                print('Instruction')
+                print('1. Write number of section')
+                print('2. Section must be number from 1')
+                print('3. Write diameter in half of section - d1/2 and lenght of section - h')
+                print('4. If you want to end of measurement write 0 in section')
+                print(dendro_formulas.sekVHub(sek='Run', d1_2=0, h=0))
+                module = 'B'
+            elif mode == 'B':
+                print('Instruction')
+                print('1. Write number of section')
+                print('2. Section must be number from 1')
+                print('3. Write base diameter of section - d0 (cm), write top diameter of section = dl (cm) '
+                      'and lenght of section = h (m)')
+                print('4. If you want to end of measurement write 0 in section')
+                print(dendro_formulas.sekVSmal(sek='Run', d0=0, dl=0, h=0))
+                module = 'B'
+            elif mode == 'X':
+                module = 'Run'
+        elif module == 'X':
+            break
 
 
-print(start)
+print(main)
